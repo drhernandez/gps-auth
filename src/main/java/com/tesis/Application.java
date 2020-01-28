7969a5fafc11e0270433e7383375150ae6b51023
Diego Hernández <drhernandez92@gmail.com>
2020-01-28 20:24:55 -0300

    commit inicial

---
+++ src/main/java/com/tesis/Application.java
@@ -0,0 +1,14 @@
+package com.tesis;
+
+import com.tesis.config.SpringConfig;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(SpringConfig.class).registerShutdownHook(true)
+                .run(args);
+    }
+}
---
+++ src/main/java/com/tesis/config/SpringConfig.java
@@ -0,0 +1,28 @@
+package com.tesis.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@ComponentScan("com.tesis")
+@Configuration
+@EnableAutoConfiguration
+public class SpringConfig implements WebMvcConfigurer {
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        return objectMapper;
+    }
+}
